778019d33f3cca8cae3fff34943f9aca541e162e
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-02-04 17:41:59 +0900

    Avoid to happen a broken pipe error in fake-app
    HAProxy may close a socket when the status code is returned by socat.
    A broken pipe error happens since socat sends subsequent bytes to the
    closed socket. To avoid the problem, socat will only send back the
    status code.

--- examples/aws_ec2/fake-app.sh
+++ examples/aws_ec2/fake-app.sh
@@ -13,15 +13,11 @@ socat -v -v tcp4-listen:9001,reuseaddr,fork,crlf system:'
 ' &
 
 # ヘルスチェックエンドポイント
+# HAProxy と組み合わせて動かすとステータスコードを返した時点でソケットがクローズされ、
+# echo を複数使ってコンテンツを返そうとすると Broken Pipe エラーが発生した。
+# 対策として、ステータスコードのみを返す。
 socat -v -v tcp4-listen:9002,reuseaddr,fork,crlf system:'
   echo "HTTP/1.0 200 OK"
-  echo "Content-Type: text/plain"
-  echo "Date: $(date)"
-  echo "Server: ${SOCAT_SOCKADDR}:${SOCAT_SOCKPORT}"
-  echo "Client: ${SOCAT_PEERADDR}:${SOCAT_PEER_PORT}"
-  echo "Connection: close"
-  echo
-  echo "OK"
 ' &
 
 wait
