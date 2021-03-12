c6e5611ccea8ae93e59dc50c64e97ccf28bfef5f
Taichi Yamakawa <xirckey@gmail.com>
2021-03-12 15:30:40 +0900

    Use 'modules/services/redhat/*' instead of  'modules/services/centos/*'

--- bin/check.sh
+++ bin/check.sh
@@ -4,14 +4,14 @@ set -eux
 
 terraform fmt -recursive -check
 
-bin/validate-tf.sh modules/service/centos/core/
-bin/validate-tf.sh modules/service/centos/dev/
+bin/validate-tf.sh modules/service/redhat/core/
+bin/validate-tf.sh modules/service/redhat/dev/
 bin/validate-tf.sh modules/platform/aws/ec2/
 bin/validate-tf.sh examples/aws_ec2/
 bin/validate-tf.sh examples/aws_vpc/
 
-bin/generate-variables-example.sh check-module-template   modules/service/centos/core/variables.tf
-bin/generate-variables-example.sh check-module-template   modules/service/centos/dev/variables.tf
+bin/generate-variables-example.sh check-module-template   modules/service/redhat/core/variables.tf
+bin/generate-variables-example.sh check-module-template   modules/service/redhat/dev/variables.tf
 bin/generate-variables-example.sh check-module-template   modules/platform/aws/ec2/variables.tf
 bin/generate-variables-example.sh check-tfvars            examples/aws_ec2/variables.tf
 bin/generate-variables-example.sh check-tfvars            examples/aws_vpc/variables.tf
