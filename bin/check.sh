09b78aed72ea6ae3cd60a3d148e883814c8ea0d6
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-03-15 11:24:48 +0900

    WIP

--- bin/check.sh
+++ bin/check.sh
@@ -10,8 +10,9 @@ bin/validate-tf.sh modules/platform/aws/ec2/
 bin/validate-tf.sh examples/aws_ec2/
 bin/validate-tf.sh examples/aws_vpc/
 
-bin/generate-variables-example.sh check-module-template   modules/service/redhat/core/variables.tf
-bin/generate-variables-example.sh check-module-template   modules/service/redhat/dev/variables.tf
-bin/generate-variables-example.sh check-module-template   modules/platform/aws/ec2/variables.tf
-bin/generate-variables-example.sh check-tfvars            examples/aws_ec2/variables.tf
-bin/generate-variables-example.sh check-tfvars            examples/aws_vpc/variables.tf
+bin/generate-variables-example.sh debug modules/service/redhat/core/variables.tf
+# bin/generate-variables-example.sh check-module-template   modules/service/redhat/core/variables.tf
+# bin/generate-variables-example.sh check-module-template   modules/service/redhat/dev/variables.tf
+# bin/generate-variables-example.sh check-module-template   modules/platform/aws/ec2/variables.tf
+# bin/generate-variables-example.sh check-tfvars            examples/aws_ec2/variables.tf
+# bin/generate-variables-example.sh check-tfvars            examples/aws_vpc/variables.tf
